bc4756424614a1ff66d5f0aeb8aba1e16752c2c9
Arman Uguray <arman.uguray@gmail.com>
2020-03-31 20:19:57 -0700

    Initial Commit

---
+++ Renderer macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Renderer macOS
+//
+//  Created by Arman Uguray on 3/31/20.
+//  Copyright © 2020 Arman Uguray. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
